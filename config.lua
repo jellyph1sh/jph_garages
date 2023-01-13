e47ade896873603b1eb909cff1203b022fe9ca44
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-01-13 14:59:36 +0100

    :wrench: | Setup configuration file.

--- config.lua
+++ config.lua
@@ -1,1 +1,14 @@
 Config = {}
+
+Config.Garages = {
+    Paleto = {
+        SpawnCarPoint = vector4(0.0, 0.0, 0.0, 0.0),
+        SpawnHeliPoint = vector4(0.0, 0.0, 0.0, 0.0),
+        Vehicles = {
+            {number=1, model="206gn", vehId=nil},
+            {number=1, model="meganers", vehId=nil},
+            {number=2, model="r1200rtgn", vehId=nil},
+            {number=2, model="expertgn", vehId=nil}
+        }
+    }
+}
