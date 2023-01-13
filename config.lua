7fce1a09ef2626f1722b6e3c80c579c8bc63f29a
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-01-13 15:40:41 +0100

    :wrench: | Update config file.

--- config.lua
+++ config.lua
@@ -5,10 +5,11 @@ Config.Garages = {
         SpawnCarPoint = vector4(0.0, 0.0, 0.0, 0.0),
         SpawnHeliPoint = vector4(0.0, 0.0, 0.0, 0.0),
         Vehicles = {
-            {number=1, model="206gn", vehId=nil},
-            {number=1, model="meganers", vehId=nil},
-            {number=2, model="r1200rtgn", vehId=nil},
-            {number=2, model="expertgn", vehId=nil}
+            {name = "Peugeot 206", number=1, model="206gn", vehId={}},
+            {name = "Renault Megane RS III", number=1, model="meganers", vehId={}},
+            {name = "BMW R1200RT", number=2, model="r1200rtgn", vehId={}},
+            {name = "Peugeot Expert", number=2, model="expertgn", vehId={}},
+            {name = "Eurocopter 145c", number=1, model="ec145gn", vehId={}}
         }
     }
 }
